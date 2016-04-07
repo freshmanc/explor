43ae4ef68eb706fa8ae3e88560b7cb8cd0a4d239
freshmanc <ming.zhu.1983@gmail.com>
2016-04-06 21:05:21 -0400

    create test for functor failures

---
+++ src/test/TestFunctorFailures.java
@@ -0,0 +1,216 @@
+package test;
+
+import persistence.*;
+import structures.*;
+import utilities.*;
+import operation.*;
+import structures.Process;
+
+
+public class TestFunctorFailures {
+	public static void main(String args[])
+	{
+		TestFunctorFailures.testSameEventsDeterministicDesignSameImp();
+		TestFunctorFailures.testSameEventsNonDeterministicDesignSameImp();
+		TestFunctorFailures.testSameEventsDeterministicDesignMore();
+		TestFunctorFailures.testSameEventsDeterministicImpMore();
+		TestFunctorFailures.testSameEventsNonDeterministicDesignMore();
+		TestFunctorFailures.testSameEventsNonDeterministicImpMore();
+//		TransitionSystem ts1=new TransitionSystem();
+//		ts1.add(new Transition("0","coin","1"));
+//		ts1.add(new Transition("1","pepsi","2"));
+//		ts1.add(new Transition("1","coke","3"));
+//		ts1.add(new Transition("1","tea","4"));
+//		ts1.add(new Transition("4","tea","6"));
+//		
+//		Process vmi1=new Process(ts1);
+//		
+//		TransitionSystem ts2=new TransitionSystem();
+//		ts2.add(new Transition("0","coin","1"));
+//		ts2.add(new Transition("1","pepsi","2"));
+//		ts2.add(new Transition("1","coke","3"));
+//		  ts2.add(new Transition(1,"tea",4));
+//		
+//		Process vmi2=new Process(ts2);
+//		CategoryProcess cvmi1=new CategoryProcess(vmi1);
+//		//Utilities.printCategory(cvmi1.getInit(), 0);
+//		CategoryProcess cvmi2=new CategoryProcess(vmi2);
+//		
+//		FunctorFailures cf=new FunctorFailures();
+//		System.out.println(cf.compareCategories(cvmi2, cvmi1));
+//		for(Iterator<Map.Entry<Trace,Trace>> it=cf.entrySet().iterator();it.hasNext();)
+//		{
+//			Map.Entry<Trace,Trace> et=it.next();
+//			System.out.println("key "+et.getKey());
+//			System.out.println("value "+et.getValue());
+//		}
+	}
+	
+	public static void testSameEventsDeterministicDesignSameImp()
+	{
+		TransitionSystem ts1=new TransitionSystem(); //implement
+		ts1.add(new Transition("0","coin","1"));
+		ts1.add(new Transition("1","pepsi'","2"));
+		ts1.add(new Transition("1","coke'","3"));
+		ts1.add(new Transition("1","tea'","4"));
+		Process vmi1=new Process(ts1);
+		//Utilities.printProcess(vmi1);
+		//System.out.println();
+		
+		TransitionSystem ts2=new TransitionSystem();//design
+		ts2.add(new Transition("0","coin","1"));
+		ts2.add(new Transition("1","pepsi'","2"));
+		ts2.add(new Transition("1","coke'","3"));
+		ts2.add(new Transition("1","tea'","4"));
+		Process vmi2=new Process(ts2);
+		//Utilities.printProcess(vmi2);
+		//System.out.println();
+		
+		CategoryProcess cvm1=new CategoryProcess(vmi1);
+		CategoryProcess cvm2=new CategoryProcess(vmi2);
+		
+		FunctorFailures cf=new FunctorFailures();
+		System.out.println("same events deterministic design same as implementation: "+cf.compareCategories(cvm2, cvm1));
+	}
+	
+	public static void testSameEventsNonDeterministicDesignSameImp()
+	{
+		TransitionSystem ts1=new TransitionSystem();//implement
+		ts1.add(new Transition("0","coin","1"));
+		ts1.add(new Transition("1","pepsi","2"));
+		ts1.add(new Transition("1","coke","3"));
+		ts1.add(new Transition("1","tea","4"));
+		Process vmi1=new Process(ts1);
+		//Utilities.printProcess(vmi1);
+		//System.out.println();
+		
+		TransitionSystem ts2=new TransitionSystem();//design
+		ts2.add(new Transition("0","coin","1"));
+		ts2.add(new Transition("1","pepsi","2"));
+		ts2.add(new Transition("1","coke","3"));
+		ts2.add(new Transition("1","tea","4"));
+		Process vmi2=new Process(ts2);
+		//Utilities.printProcess(vmi2);
+		//System.out.println();
+		
+		CategoryProcess cvm1=new CategoryProcess(vmi1);
+		CategoryProcess cvm2=new CategoryProcess(vmi2);
+		
+		FunctorFailures cf=new FunctorFailures();
+		System.out.println("same events nondeterministic design same as implementation: "+cf.compareCategories(cvm2, cvm1));
+	}
+	
+	public static void testSameEventsNonDeterministicImpMore()
+	{
+		TransitionSystem ts1=new TransitionSystem();//implement
+		ts1.add(new Transition("0","coin","1"));
+		ts1.add(new Transition("1","pepsi","2"));
+		ts1.add(new Transition("1","coke","3"));
+		ts1.add(new Transition("1","tea","4"));
+		ts1.add(new Transition("1","coin","5"));
+		Process vmi1=new Process(ts1);
+		//Utilities.printProcess(vmi1);
+		//System.out.println();
+		
+		TransitionSystem ts2=new TransitionSystem();//design
+		ts2.add(new Transition("0","coin","1"));
+		ts2.add(new Transition("1","pepsi","2"));
+		ts2.add(new Transition("1","coke","3"));
+		ts2.add(new Transition("1","tea","4"));
+		
+		Process vmi2=new Process(ts2);
+		//Utilities.printProcess(vmi2);
+		//System.out.println();
+		
+		CategoryProcess cvm1=new CategoryProcess(vmi1);
+		CategoryProcess cvm2=new CategoryProcess(vmi2);
+		
+		FunctorFailures cf=new FunctorFailures();
+		System.out.println("same events nondeterministic implementation more than design: "+cf.compareCategories(cvm2, cvm1));		
+	}
+	
+	public static void testSameEventsNonDeterministicDesignMore()
+	{
+		TransitionSystem ts1=new TransitionSystem();//implement
+		ts1.add(new Transition("0","coin","1"));
+		ts1.add(new Transition("1","pepsi","2"));
+		ts1.add(new Transition("1","coke","3"));
+		ts1.add(new Transition("1","tea","4"));
+		Process vmi1=new Process(ts1);
+//		Utilities.printProcess(vmi1);
+//		System.out.println();
+		
+		TransitionSystem ts2=new TransitionSystem();//design
+		ts2.add(new Transition("0","coin","1"));
+		ts2.add(new Transition("1","pepsi","2"));
+		ts2.add(new Transition("1","coke","3"));
+		ts2.add(new Transition("1","tea","4"));
+		ts2.add(new Transition("1","coin","5"));
+		Process vmi2=new Process(ts2);
+//		Utilities.printProcess(vmi2);
+//		System.out.println();
+		
+		CategoryProcess cvm1=new CategoryProcess(vmi1);
+		CategoryProcess cvm2=new CategoryProcess(vmi2);
+		
+		FunctorFailures cf=new FunctorFailures();
+		System.out.println("same events nondeterministic design more than implementation: "+cf.compareCategories(cvm2, cvm1));		
+	}
+	
+	public static void testSameEventsDeterministicImpMore()
+	{
+		TransitionSystem ts1=new TransitionSystem();//implement
+		ts1.add(new Transition("0","coin","1"));
+		ts1.add(new Transition("1","pepsi'","2"));
+		ts1.add(new Transition("1","coke'","3"));
+		ts1.add(new Transition("1","tea'","4"));
+		ts1.add(new Transition("1","coin'","5"));
+		Process vmi1=new Process(ts1);
+//		Utilities.printProcess(vmi1);
+//		System.out.println();
+		
+		TransitionSystem ts2=new TransitionSystem();//design
+		ts2.add(new Transition("0","coin","1"));
+		ts2.add(new Transition("1","pepsi'","2"));
+		ts2.add(new Transition("1","coke'","3"));
+		ts2.add(new Transition("1","tea'","4"));
+		Process vmi2=new Process(ts2);
+//		Utilities.printProcess(vmi2);
+//		System.out.println();
+		
+		CategoryProcess cvm1=new CategoryProcess(vmi1);
+		CategoryProcess cvm2=new CategoryProcess(vmi2);
+		
+		FunctorFailures cf=new FunctorFailures();
+		System.out.println("same events deterministic implementation more than design: "+cf.compareCategories(cvm2, cvm1));		
+	}
+	
+	public static void testSameEventsDeterministicDesignMore()
+	{
+		TransitionSystem ts1=new TransitionSystem();//implement
+		ts1.add(new Transition("0","coin","1"));
+		ts1.add(new Transition("1","pepsi'","2"));
+		ts1.add(new Transition("1","coke'","3"));
+		ts1.add(new Transition("1","tea'","4"));	
+		Process vmi1=new Process(ts1);
+//		Utilities.printProcess(vmi1);
+//		System.out.println();
+		
+		TransitionSystem ts2=new TransitionSystem();//design
+		ts2.add(new Transition("0","coin","1"));
+		ts2.add(new Transition("1","pepsi'","2"));
+		ts2.add(new Transition("1","coke'","3"));
+		ts2.add(new Transition("1","tea'","4"));
+		ts2.add(new Transition("1","coin'","5"));
+		Process vmi2=new Process(ts2);
+//		Utilities.printProcess(vmi2);
+//		System.out.println();
+		
+		CategoryProcess cvm1=new CategoryProcess(vmi1);
+		CategoryProcess cvm2=new CategoryProcess(vmi2);
+		
+		FunctorFailures cf=new FunctorFailures();
+		System.out.println("same events deterministic design more than implementation: "+cf.compareCategories(cvm2, cvm1));		
+	}
+	
+}
